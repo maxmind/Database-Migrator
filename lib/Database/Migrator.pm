bd3bc56b4236adf9c602031d0ed8c01aeacfeb04
William Storey <wstorey@maxmind.com>
2018-04-17 15:50:19 +0000

    Bump version after release

--- lib/Database/Migrator.pm
+++ lib/Database/Migrator.pm
@@ -3,7 +3,7 @@ package Database::Migrator;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 1;
 
--- lib/Database/Migrator/Types.pm
+++ lib/Database/Migrator/Types.pm
@@ -3,7 +3,7 @@ package Database::Migrator::Types;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 use MooseX::Types::Moose;
 use MooseX::Types::Path::Class;
